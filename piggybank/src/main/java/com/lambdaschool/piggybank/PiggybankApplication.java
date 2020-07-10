95e8724f7a28bed330473714f572e635a1e92f0b
marti <Martinezm0087@gmail.com>
2020-07-10 16:43:32 -0400

    mvp

---
+++ piggybank/src/main/java/com/lambdaschool/piggybank/PiggybankApplication.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.piggybank;
+
+import com.lambdaschool.piggybank.models.Coin;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggybankApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PiggybankApplication.class, args);
+
+    }
+
+}
